4dee0432fc2b84c917bbfda46bb3766e4df7b157
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-02-27 12:49:46 +0530

    random Color genrator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Accordian from './components/accordian'
+import RandomColor from './components/random-color'
 
 function App() {
 
 
   return (
     <>
-    <Accordian/>
+    {/* Accordian */}
+    {/* <Accordian/> */}
+    {/* random Color */}
+    <RandomColor/>
     </>
   )
 }
