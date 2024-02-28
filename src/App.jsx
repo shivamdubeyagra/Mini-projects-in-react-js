fba96546765c103bc5d216eaaeee58858fe5cdab
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-02-28 22:16:10 +0530

    image-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Accordian from './components/accordian'
+import ImageSlider from './components/image-slider'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 
@@ -11,7 +12,8 @@ function App() {
     <>
     {/* <Accordian/> */}
     {/* <RandomColor/> */}
-    <StarRating noOfStars={10}/>
+    {/* <StarRating noOfStars={10}/> */}
+    <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/>
     </>
   )
 }
