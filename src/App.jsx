1ec324ad49e8393e0e715b60bd275f787bb419c5
shivamdubeyagra <shivamdubeyagra@gmail.com>
2024-02-29 23:15:50 +0530

    Theme changer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Accordian from './components/accordian'
 import ImageSlider from './components/image-slider'
+import LightDarkMode from './components/light-dark-mode/index.jsx'
 import LoadMoreData from './components/load-more-data'
 import QRCodeGenerator from './components/qr-code/index.jsx'
 import RandomColor from './components/random-color'
@@ -20,7 +21,8 @@ function App() {
     {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/> */}
     {/* <LoadMoreData/> */}
     {/* <TreeView menus={menus}/> */}
-    <QRCodeGenerator/>
+    {/* <QRCodeGenerator/> */}
+    <LightDarkMode/>
     </>
   )
 }
